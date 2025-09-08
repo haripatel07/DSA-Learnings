a35e2ab08414345d0aab49fa8aa535a6fdb7537d
Hari Patel <patelhariv18@gmail.com>
2025-09-08 11:17:35 +0530

    Added solution to GFG POTD, 8th September, 2025

---
+++ GFG/POTD/08_09_2025.cpp
@@ -0,0 +1,58 @@
+/*
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    Node* merge(Node* left, Node* right) {
+        if (!left) return right;
+        if (!right) return left;
+
+        Node* result = NULL;
+
+        if (left->data <= right->data) {
+            result = left;
+            result->next = merge(left->next, right);
+        } else {
+            result = right;
+            result->next = merge(left, right->next);
+        }
+        return result;
+    }
+
+    Node* getMiddle(Node* head) {
+        if (head == NULL || head->next == NULL) 
+            return head;
+
+        Node* slow = head;
+        Node* fast = head->next;
+
+        while (fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+
+    Node* mergeSort(Node* head) {
+        if (!head || !head->next) 
+            return head;
+
+        Node* mid = getMiddle(head);
+        Node* rightHead = mid->next;
+        mid->next = NULL;
+        Node* left = mergeSort(head);
+        Node* right = mergeSort(rightHead);
+
+        return merge(left, right);
+    }
+};
