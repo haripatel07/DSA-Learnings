29b27dd4ab8f17aa3f17d3708863e6b877b836c7
Hari Patel <patelhariv18@gmail.com>
2025-10-04 21:05:20 +0530

    Added leetcode solution to 4th October, 2025

---
+++ LeetCode/Medium/11_04_10_2025.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int l = 0;
+        int r = height.size() - 1;
+        int area = 0;
+        while (l <= r){
+            area = max(min(height[l], height[r]) * (r - l), area);
+            if (height[l] < height[r]) l++;
+            else r--;
+        }
+        return area;
+    }
+};
